1a0d91678bf013ecbb4d352a543da7caeac8f6ca
Ben Booth <bkbooth@gmail.com>
2019-10-09 10:39:06 +1100

    Tweak app branding

--- src/App.jsx
+++ src/App.jsx
@@ -6,7 +6,7 @@ function App() {
       <header className="bg-gray-800 text-gray-100 min-h-screen flex flex-col items-center justify-center">
         <img src={`${process.env.PUBLIC_URL}/logo256.png`} alt="ICON Foundation logo" />
         <h1 className="text-xl md:text-2xl lg:text-3xl">
-          My <span className="text-cyan">ICON</span> Wallet
+          my<span className="text-cyan">ICON</span>wallet
         </h1>
       </header>
     </div>
