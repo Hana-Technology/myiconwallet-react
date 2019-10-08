5521f5fd5bb1718e2add3dfdee398649864b9de0
Ben Booth <bkbooth@gmail.com>
2019-10-08 13:45:56 +1100

    Use PurgeCSS to cleanup and minify Tailwind CSS

--- postcss.config.js
+++ postcss.config.js
@@ -3,5 +3,12 @@ module.exports = {
     require('postcss-import'),
     require('tailwindcss'),
     require('postcss-preset-env')({ stage: 3 }),
+    ...(process.env.NODE_ENV === 'production'
+      ? [
+          require('@fullhuman/postcss-purgecss')({
+            content: ['public/index.html', 'src/**/*.jsx'],
+          }),
+        ]
+      : []),
   ],
 };
