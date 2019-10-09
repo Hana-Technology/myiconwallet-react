df067d2e13d2e447f4b006e7d45964b851239967
Ben Booth <bkbooth@gmail.com>
2019-10-10 00:36:22 +1100

    Basic styling for the not found page

--- src/pages/NotFound.jsx
+++ src/pages/NotFound.jsx
@@ -1,10 +1,18 @@
 import React from 'react';
+import { Link } from '@reach/router';
 import Layout from 'components/Layout';
 
 function NotFound() {
   return (
     <Layout>
-      <p className="text-lg">Page not found</p>
+      <h2 className="text-2xl uppercase tracking-tight mb-2">Page not found</h2>
+      <p>
+        Sorry, we couldn't find the page you're looking for. Back to{' '}
+        <Link to="/" className="text-teal-600 hover:text-teal-800 focus:text-teal-800">
+          home
+        </Link>
+        .
+      </p>
     </Layout>
   );
 }
