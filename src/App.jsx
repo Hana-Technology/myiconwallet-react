175dd9933b9e31a927216814bae91d0a827b379a
Ben Booth <bkbooth@gmail.com>
2019-10-09 11:26:31 +1100

    Add a logo component which renders an SVG

--- src/App.jsx
+++ src/App.jsx
@@ -1,12 +1,14 @@
 import React from 'react';
+import Logo from 'components/Logo';
 
 function App() {
   return (
     <div className="text-center">
       <header className="bg-gray-800 text-gray-100 min-h-screen flex flex-col items-center justify-center">
-        <img src={`${process.env.PUBLIC_URL}/logo256.png`} alt="ICON Foundation logo" />
-        <h1 className="text-xl md:text-2xl lg:text-3xl">
-          my<span className="text-cyan">ICON</span>wallet
+        <h1 className="text-xl md:text-2xl lg:text-3xl whitespace-no-wrap">
+          my
+          <Logo className="text-cyan mx-px md:mx-1" />
+          wallet
         </h1>
       </header>
     </div>
