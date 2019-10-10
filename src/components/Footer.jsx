cff1cc48e7f2fa82bfe25c21d84802a2977eb86d
Ben Booth <bkbooth@gmail.com>
2019-10-10 20:54:41 +1100

    Slightly darker footer background

--- src/components/Footer.jsx
+++ src/components/Footer.jsx
@@ -2,7 +2,7 @@ import React from 'react';
 
 function Footer() {
   return (
-    <footer className="bg-gray-200 text-gray-600 p-4">
+    <footer className="bg-gray-300 text-gray-600 p-4">
       <div className="container mx-auto text-right text-sm">
         © {new Date().getFullYear()}{' '}
         <a className="hover:text-gray-700 focus:text-gray-700" href="https://www.reliantnode.com/">
