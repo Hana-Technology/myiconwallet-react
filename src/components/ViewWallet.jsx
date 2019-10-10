0521cd5bc224bafa767957d2849c8a4f12f506d7
Ben Booth <bkbooth@gmail.com>
2019-10-11 00:27:56 +1100

    Initial work on wallet homepage

--- src/components/ViewWallet.jsx
+++ src/components/ViewWallet.jsx
@@ -1,4 +1,5 @@
 import React from 'react';
+import Alert, { ALERT_TYPE_SUCCESS } from 'components/Alert';
 import { useWallet } from 'components/Wallet';
 
 function ViewWallet() {
@@ -6,9 +7,21 @@ function ViewWallet() {
 
   return (
     wallet && (
-      <p className="text-lg">
-        Your wallet address is <b>{wallet.getAddress()}</b>!
-      </p>
+      <>
+        <h2 className="text-2xl uppercase tracking-tight mb-2">Your wallet</h2>
+        <Alert
+          type={ALERT_TYPE_SUCCESS}
+          title={wallet.getAddress()}
+          showIcon={false}
+          className="break-all mb-8"
+        />
+        <p>TODO:</p>
+        <ul>
+          <li>Link to ICON Tracker</li>
+          <li>Show ISX and iScore</li>
+          <li>Show graphic representation</li>
+        </ul>
+      </>
     )
   );
 }
