9ef5f5afd91136f930a2fac542dc4d7e9660e413
Ben Booth <bkbooth@gmail.com>
2019-10-14 16:10:16 +1100

    First pass at adding a responsive menu

--- src/components/Header.jsx
+++ src/components/Header.jsx
@@ -1,10 +1,14 @@
-import React from 'react';
+import React, { useState } from 'react';
+import { FontAwesomeIcon } from '@fortawesome/react-fontawesome';
+import { faFlag, faShareSquare, faVoteYea } from '@fortawesome/pro-duotone-svg-icons';
+import { faHamburger } from '@fortawesome/pro-solid-svg-icons';
+
 import { Link, navigate } from '@reach/router';
 import Logo from 'components/Logo';
 import { useWallet } from 'components/Wallet';
 
 const navLinkBaseClasses =
-  'flex h-full text-center hover:text-gray-100 focus:text-gray-100 items-center p-1 mx-2 border-b-2';
+  'flex h-full text-center hover:text-gray-100 focus:text-gray-100 items-center px-3 py-1 sm:p-1 mx-2 sm:mx-2 my-1 sm:my-0 border-l-2 sm:border-b-2';
 
 function NavLink({ children, to }) {
   return (
@@ -23,6 +27,12 @@ function NavLink({ children, to }) {
 
 function Header() {
   const { wallet, unloadWallet } = useWallet();
+  const [menuIsOpen, setMenuIsOpen] = useState(false);
+
+  function toggleMenu(event) {
+    event.preventDefault();
+    setMenuIsOpen(!menuIsOpen);
+  }
 
   function handleUnloadWallet(event) {
     event.preventDefault();
@@ -31,8 +41,8 @@ function Header() {
   }
 
   return (
-    <header className="bg-gray-800 text-gray-100 p-4 pt-3 shadow-lg z-10">
-      <div className="container mx-auto flex justify-between">
+    <header className="bg-gray-800 shadow-lg z-10 relative">
+      <div className="container mx-auto p-4 pt-3 bg-gray-800 text-gray-100 flex items-center justify-between">
         <h1 className="text-2xl lg:text-3xl whitespace-no-wrap" aria-label="my icon wallet">
           <Link to="/">
             my
@@ -41,17 +51,37 @@ function Header() {
           </Link>
         </h1>
         <nav>
-          <ul className="flex items-stretch justify-start">
+          <button
+            onClick={toggleMenu}
+            className="block sm:hidden text-gray-200 hover:text-white focus:text-white p-2"
+          >
+            <FontAwesomeIcon icon={faHamburger} fixedWidth />
+          </button>
+          <ul
+            className={`absolute sm:static left-0 right-0 bg-gray-800 sm:flex sm:items-stretch sm:justify-start shadow-lg sm:shadow-none pb-3 sm:p-0 slide-down ${
+              menuIsOpen ? 'slide-down-in' : ''
+            }`}
+            style={{ top: '64px', zIndex: '-1' }}
+          >
             {wallet ? (
               <>
                 <li>
-                  <NavLink to="/send">Send</NavLink>
+                  <NavLink to="/send">
+                    <FontAwesomeIcon icon={faShareSquare} fixedWidth className="mr-1" />
+                    Send
+                  </NavLink>
                 </li>
                 <li>
-                  <NavLink to="/stake">Stake</NavLink>
+                  <NavLink to="/stake">
+                    <FontAwesomeIcon icon={faFlag} fixedWidth className="mr-1" />
+                    Stake
+                  </NavLink>
                 </li>
                 <li>
-                  <NavLink to="/vote">Vote</NavLink>
+                  <NavLink to="/vote">
+                    <FontAwesomeIcon icon={faVoteYea} fixedWidth className="mr-1" />
+                    Vote
+                  </NavLink>
                 </li>
                 <li>
                   <button
@@ -75,6 +105,13 @@ function Header() {
           </ul>
         </nav>
       </div>
+      {menuIsOpen && (
+        <div
+          className="fixed sm:hidden inset-0 bg-black opacity-25"
+          style={{ zIndex: -2 }}
+          onClick={toggleMenu}
+        ></div>
+      )}
     </header>
   );
 }
