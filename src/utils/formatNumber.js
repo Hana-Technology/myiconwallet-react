0333db00e64cfc97d79121d2091b8864de99f9cb
Ben Booth <bkbooth@gmail.com>
2019-10-15 00:48:13 +1100

    Always round down when showing ICX balances

--- src/utils/formatNumber.js
+++ src/utils/formatNumber.js
@@ -1,6 +1,10 @@
 import { IconConverter } from 'icon-sdk-js';
 import { endsWith, trimEnd } from 'lodash-es';
 
+// See BigNumber.js rounding modes
+// https://mikemcl.github.io/bignumber.js/#constructor-properties
+const ROUNDING_MODE = 1;
+
 /**
  * @param {BigNumber} value
  * @returns {string}
@@ -11,6 +15,6 @@ export function formatNumber(value, decimalPlaces = 4) {
   const minimumValue = IconConverter.toBigNumber(`0.${'0'.repeat(decimalPlaces)}1`);
   if (value.isLessThan(minimumValue)) return '~0';
 
-  const result = trimEnd(value.toFixed(decimalPlaces), '0');
+  const result = trimEnd(value.toFixed(decimalPlaces, ROUNDING_MODE), '0');
   return `${result}${endsWith(result, '.') ? '0' : ''}`;
 }
