d6d16d7f9e63f3d760e4f33d3d52e89d8450b170
Ben Booth <bkbooth@gmail.com>
2019-10-16 21:55:27 +1100

    Show insignificant balance as 0

--- src/utils/formatNumber.js
+++ src/utils/formatNumber.js
@@ -13,7 +13,7 @@ export function formatNumber(value, decimalPlaces = 4) {
   if (value.isInteger()) return value.toFixed(null);
 
   const minimumValue = IconConverter.toBigNumber(`0.${'0'.repeat(decimalPlaces)}1`);
-  if (value.isLessThan(minimumValue)) return '~0';
+  if (value.isLessThan(minimumValue)) return '0';
 
   const result = trimEnd(value.toFixed(decimalPlaces, ROUNDING_MODE), '0');
   return `${result}${endsWith(result, '.') ? '0' : ''}`;
