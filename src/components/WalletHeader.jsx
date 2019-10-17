75d1647701a770a25d988fc614e952db267fed89
Ben Booth <bkbooth@gmail.com>
2019-10-17 16:54:51 +1100

    Click wallet header address copies to clipboard

--- src/components/WalletHeader.jsx
+++ src/components/WalletHeader.jsx
@@ -1,30 +1,46 @@
-import React from 'react';
+import React, { useRef, useState } from 'react';
 import { FontAwesomeIcon } from '@fortawesome/react-fontawesome';
 import { faSync, faWallet } from '@fortawesome/pro-duotone-svg-icons';
+import ReactTooltip from 'react-tooltip';
 import { formatNumber } from 'utils/formatNumber';
-import { useIconService } from 'components/IconService';
 import { useWallet } from 'components/Wallet';
 
+const COPY_TOOLTIPS = {
+  INITIAL: 'Copy address',
+  COPIED: 'Address copied',
+};
+
 function WalletHeader() {
-  const {
-    network: { trackerUrl },
-  } = useIconService();
   const { fullBalance, isLoading, refreshWallet, wallet } = useWallet();
+  const copyTooltipRef = useRef(null);
+  const [copyTooltip, setCopyTooltip] = useState(COPY_TOOLTIPS.INITIAL);
+
+  function copyAddressToClipboard() {
+    if (navigator && navigator.clipboard) {
+      navigator.clipboard.writeText(wallet.getAddress());
+
+      // Update and re-show the tooltip
+      ReactTooltip.hide(copyTooltipRef.current);
+      setCopyTooltip(COPY_TOOLTIPS.COPIED);
+      setTimeout(() => ReactTooltip.show(copyTooltipRef.current), 50);
+      setTimeout(() => setCopyTooltip(COPY_TOOLTIPS.INITIAL), 2000);
+    }
+  }
 
   return (
     wallet && (
       <div className="bg-blue-600 text-blue-100 shadow-inner py-3 px-6 lg:px-8 -mt-6 -ml-6 -mr-6 lg:-mt-8 lg:-ml-8 lg:-mr-8">
         <div className="flex items-center justify-between">
-          <a
-            href={`${trackerUrl}/address/${wallet.getAddress()}`}
-            title="View on ICON tracker"
-            target="_blank"
-            rel="noopener noreferrer"
+          <button
+            onClick={copyAddressToClipboard}
+            data-tip={copyTooltip}
+            ref={copyTooltipRef}
             className="whitespace-no-wrap hover:text-white focus:text-white"
           >
             <FontAwesomeIcon icon={faWallet} className="mr-2" />
             {wallet.getAddress().substr(0, 6)}&hellip;{wallet.getAddress().substr(-8)}
-          </a>
+            <ReactTooltip place="right" effect="solid" />
+          </button>
           <button
             onClick={() => refreshWallet()}
             disabled={isLoading}
