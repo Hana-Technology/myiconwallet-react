b2425cab065351474b9b85d9cb72544a0986aa4d
Ben Booth <ben@benbooth.dev>
2020-05-25 23:35:16 +1000

    Add a homepage banner to the app marketing page

---
+++ src/components/AppBanner.jsx
@@ -0,0 +1,25 @@
+import React from 'react';
+import { FontAwesomeIcon } from '@fortawesome/react-fontawesome';
+import { faAngleRight } from '@fortawesome/free-solid-svg-icons';
+
+function AppBanner() {
+  return (
+    <div className="relative z-50 bg-miw-dark text-center p-3">
+      <a
+        href="/app"
+        className="p-2 bg-miw-primary30 items-center text-white leading-none rounded-full inline-flex"
+        role="alert"
+      >
+        <span className="flex rounded-full bg-miw-primary uppercase px-2 py-1 text-xs font-semibold">
+          New
+        </span>
+        <span className="text-left flex-auto mx-3">
+          Checkout the new MyIconWallet app on iPhone and Android
+        </span>
+        <FontAwesomeIcon icon={faAngleRight} className="text-sm" />
+      </a>
+    </div>
+  );
+}
+
+export default AppBanner;
--- src/pages/Home.jsx
+++ src/pages/Home.jsx
@@ -1,4 +1,5 @@
 import React from 'react';
+import AppBanner from 'components/AppBanner';
 import Landing from 'components/Landing';
 import Layout from 'components/Layout';
 import ViewWallet from 'components/ViewWallet';
@@ -7,7 +8,12 @@ import { useWallet } from 'components/Wallet';
 function HomePage() {
   const { wallet } = useWallet();
 
-  return <Layout styleMain={!!wallet}>{wallet ? <ViewWallet /> : <Landing />}</Layout>;
+  return (
+    <>
+      {!wallet && <AppBanner />}
+      <Layout styleMain={!!wallet}>{wallet ? <ViewWallet /> : <Landing />}</Layout>
+    </>
+  );
 }
 
 export default HomePage;
